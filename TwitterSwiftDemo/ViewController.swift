97b0a46333c4dd0752f8ae9acc4773eb8114e34c
miracolo131 <miracolo0131@gmail.com>
2015-01-05 13:57:44 +0800

    Initial Commit

---
+++ TwitterSwiftDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TwitterSwiftDemo
+//
+//  Created by Miracolo Bosco on 15/1/5.
+//  Copyright (c) 2015年 MI-31. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
